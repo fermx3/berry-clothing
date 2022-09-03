b67fbe7d0b994b743764c15f59fca2ab51579f2d
Fernando Rios <fernandomanuelriosalcantara@iMac-de-Fernando.local>
2022-09-03 16:41:29 -0500

    Cart dropdown feature & cart icon count feature complete

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,28 @@
-import Button from '../button/button.component';
+import { useContext } from "react";
 
-import './cart-dropdown.styles.scss';
+import { CartContext } from "../../contexts/cart.context";
+
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+
+import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
-    return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                <Button>GO TO CHECKOUT</Button>
-            </div>
-        </div>
-    )
-}
+  const { cartItems } = useContext(CartContext);
+
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+      {cartItems.length !== 0 ? 
+        cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        )) : 
+        <span>Add Items to Cart</span>
+      }
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CartDropdown;
